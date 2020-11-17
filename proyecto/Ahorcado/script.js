78977a7ca7372e4dc2909afaabda813db64463d4
Alanis <alanis.crln@gmail.com>
2020-11-17 18:43:58 +0000

    ahorcado avanzado un poco y algo de teoria

--- proyecto/Ahorcado/script.js
+++ proyecto/Ahorcado/script.js
@@ -9,13 +9,54 @@ let posLetra = new Array(); // array para ir guardando las posiciones de la letr
 
 
 class Productos{
-    constructor(titulo){
+    constructor(titulo, anioLanzamiento, director, actores, poster){
         this.titulo = titulo;
+        this.anioLanzamiento = anioLanzamiento;
+        this.director = director;
+        this.actores = actores;
+        this.poster = poster;
     }
 
     getTitulo(){
-        return thid.titulo;
+        return this.titulo;
     }
+
+    getAnioLanzamiento(){
+        return this.anioLanzamiento;
+    }
+
+    getDirector(){
+        return this.director;
+    }
+
+    getActores(){
+        return this.actores;
+    }
+
+    getPoster(){
+        return this.poster;
+    }
+
+    setTitulo(titulo){
+        this.titulo = titulo;
+    }
+
+    setAnioLanzamiento(anioLanzamiento){
+        this.anioLanzamiento = anioLanzamiento;
+    }
+
+    setDirector(director){
+        this.director = director;
+    }
+
+    setActores(actores){
+        this.actores = actores;
+    }
+
+    setPoster(poster){
+        this.poster = poster;
+    }
+
 
 
 }
@@ -80,5 +121,73 @@ function pintarLetra( letra ){
         palabraMostrada = setCharAt(palabraMostrada, posLetra[i], letra);
     }
 }
+
+// PARTIDA DOS JUGADORES:
+
+/*
+Implementar la dinámica de juego completa con dos jugadores (de forma local) y el número de partidas a jugar.
+
+Guardar tanto en cookies como en webstorage: el usuario, el número de victorias y el número de partidas realizadas. 
+Se tiene que poder visualizar esta información en algún momento, así como eliminarla.
+
+Implementar las clases necesarias para el desarrollo completo del juego, mínimo se deben crear dos, con sus propiedades y métodos. 
+
+Añadir información extra a producto: año de lanzamiento, director, actores, poster e imágenes. 
+
+*/
+
+
+/*
+
+se guarda en cookies y en webstorage simplemente para poder ir tabajandolas, pero lo logico seria solo usar una, en este caso
+webtorage.
+
+IDEA:
+
+se guardan los dos jugadores  (cookies y webstorage). 
+se solicita el num de rondas.
+jugador1 introduce la primera palabra
+jugador2 juega.
+se dice si jugador 2 gano o perdio
+jugador2 introduce otra palbra
+juega jugador1
+.
+.
+.
+.
+al terminar todas las rondas, se dice quien ha ganado
+se pregunta si se quiere volver a jugar.
+si se juega de nuevo, se borran los datos y se vuelve al principio
+si se quiere dejar de jugar, se borran los datos y se muestra mensaje
+
+
+*/
+
+
+//por mi crearia una clase jugador, pero el profe quiere q se guarde en cookies y en webstorage... asi q no se si crearla o no
+
+function guardarJugador(jugador){
+
+    if(document.cookie == null){
+        document.cookie = "jugador1 = " + jugador;
+        localStorage.setItem("jugador1", jugador);        
+    }else{
+        document.cookie = "jugador2 = " + jugador;
+        localStorage.setItem("jugador2", jugador);
+    }
+
+}
+
+function guardarVictoriaJug1(jugador, numVictorias){
+    document,cookie = "jugador1 = " + jugador + "; victorias = " + numVictorias;
+    localStorage.setItem("victoriasJugador1", numVictorias);
+}
+
+
+function guardarVictoriaJug2(jugador, numVictorias){
+    document,cookie = "jugador2 = " + jugador + "; victorias = " + numVictorias;
+    localStorage.setItem("victoriasJugador2", numVictorias);
+}
+
 
 
