5a991a2bc4c969f6a4c8ab351bad9963adc70ec1
Alanis <alanis.crln@gmail.com>
2020-11-19 02:56:15 +0000

    teclado terminado

--- proyecto/Ahorcado/teclado/teclado.js
+++ proyecto/Ahorcado/teclado/teclado.js
@@ -1,137 +1,68 @@
-const teclado = {
+const teclado = {   //elemento teclado
     elementos: {
-        main: null,
-        contenedorTeclas: null,
+        containerTeclado: null,
+        containerTeclas: null,
         teclas: []
     },
-
-    eventHandlers: {
-        oninput: null,
-        onclose: null
-    },
-
     propiedades: {
-        valor: "",
-        mayus: false
+        valor: ""   // aqui va la letra
     },
 
-    inicio() {
-        //crear los elemetos o
-        this.elementos.main = document.createElement("div");
-        this.elementos.contenedorTeclas = document.createElement("div");
+    // metodos
 
-        this.elementos.main.classList.add("teclado");
-        this.elementos.contenedorTeclas.classList.add("teclado-teclas");
-        this.elementos.contenedorTeclas.appendChild(this.crearTeclas());
-        this.elementos.teclas = this.elementos.contenedorTeclas.querySelectorAll(".tecla");
+    init() {
+        // crear contenedores
+        this.elementos.containerTeclado = document.createElement("div");
+        this.elementos.containerTeclado.classList.add("teclado");
+        this.elementos.containerTeclas = document.createElement("div");
+        this.elementos.containerTeclas.classList.add("container-teclas");
 
+        //crear la estructura
+        document.body.appendChild(this.elementos.containerTeclado);
+        this.elementos.containerTeclado.appendChild(this.elementos.containerTeclas);
+        this.elementos.containerTeclas.appendChild(this.crearTeclas());
 
-
-        this.elementos.main.appendChild(this.elementos.contenedorTeclas);
-        document.body.appendChild(this.elementos.main);
-
-
-        document.querySelectorAll(".usa-teclado").forEach(element => {
-            element.addEventListener("focus", () => {
-                this.open(element.value, currentValue => {
-                    element.value = currentValue;
-                });
-            });
-        });
+        this.elementos.teclas = this.elementos.containerTeclas.querySelectorAll(".tecla");
 
     },
 
     crearTeclas() {
+
+        //fragment es un doc pequeño donde se guarda mucha info, para cuando
+        //ya este todo guardado en ella, empujarlo de una al html. asi se evita el reflow
+        //Reflow sucede cuando un nav debe procesar y pintar parte de una web nuevamente (f5)
         const fragment = document.createDocumentFragment();
-        const teclaLayout = [
-            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "?", "¿", "backspace",
+        const letras = [
             "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
             "a", "s", "d", "f", "g", "h", "j", "k", "l", "ñ",
-            "z", "x", "c", "v", "b", "n", "m", ",", ".",
-            "space"
+            "z", "x", "c", "v", "b", "n", "m"
         ];
 
-        //funcion q crea html para un icon
+        letras.forEach(letra => {
+            btn = document.createElement("button");
 
-        const creaIconoHTML = (nombreIcono) => {
-            return `<i class="material-icons">${nombreIcono}</i>`;
-        };
+            btn.setAttribute("type", "button");
+            btn.classList.add("tecla");
 
-        teclaLayout.forEach(tecla => {
-            const teclaElemento = document.createElement("button");
-            const insertSalto = ["backspace", "p", "ñ", "."].indexOf(tecla) !== -1;
+            btn.textContent = letra;
+            btn.addEventListener("click", () => {
+                this.propiedades.valor = letra;
+                //escribir letra en la zona de texto
+                zonaTexto = document.querySelector(".usa-teclado");
+                zonaTexto.textContent = this.propiedades.valor;
+            });
 
-            //añadir clases a tecla
+            fragment.appendChild(btn);  // guardamos en fragment cada btn de letra
 
-            teclaElemento.setAttribute("type", "button");
-            teclaElemento.classList.add("tecla");
-
-            switch (tecla) {
-                case "backspace":
-                    teclaElemento.innerHTML = creaIconoHTML("backspace");
-
-                    teclaElemento.addEventListener("click", () => {
-                        this.propiedades.valor = this.propiedades.valor.substring(0, this.propiedades.valor.length - 1);
-                        this.triggerEvent("oninput");
-                    });
-                    break;
-
-                case "space":
-                    teclaElemento.classList.add("tecla-ancha");
-                    teclaElemento.innerHTML = creaIconoHTML("space_bar");
-
-                    teclaElemento.addEventListener("click", () => {
-                        this.propiedades.valor += " ";
-                        this.dispartriggerEventadorEvento("oninput");
-                    });
-                    break;
-
-                default:
-                    teclaElemento.textContent = tecla.toLowerCase();
-                    teclaElemento.addEventListener("click", () => {
-                        this.propiedades.valor += tecla;
-                        this.triggerEvent("oninput");
-                    });
-                    
-            }
-
-            fragment.appendChild(teclaElemento);
-
-            if(insertSalto){
+            if (letra == "p" || letra == "ñ") {   // mostrar teclado en filas
                 fragment.appendChild(document.createElement("br"));
             }
 
         });
 
         return fragment;
-    },
-
-    triggerEvent(handlerName) {
-        if (typeof this.eventHandlers[handlerName] == "function") {
-            this.eventHandlers[handlerName](this.propiedades.valor);
-        }
-    },
-
-    disparadorEvento(handlerEvent) {
-        console.log("evento " + handlerEvent + " lanzado");
-    },
-
-    mayusActivado() {
-        console.log("mayuda activas");
-    },
-
-    open(valorInicial, oninput, onclose) {
-        this.propiedades.valor = valorInicial || "";
-        this.eventHandlers.oninput = oninput;
-        this.eventHandlers.onclose = onclose;
-    },
-
-    close() {
-
     }
 
 };
 
-window.addEventListener("DOMContentLoaded", function () {
-    teclado.inicio();
-});
+window.addEventListener("load", teclado.init());
