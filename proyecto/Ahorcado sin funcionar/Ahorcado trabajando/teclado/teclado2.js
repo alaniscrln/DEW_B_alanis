b37c7e5be0af1b78c27f83fd26e1bdcbaa9dae27
Alanis <alanis.crln@gmail.com>
2021-01-11 13:09:20 +0000

    proyecto ahorcado avanzando

---
+++ proyecto/Ahorcado sin funcionar/Ahorcado trabajando/teclado/teclado2.js
@@ -0,0 +1,127 @@
+const teclado = {   //elemento teclado
+    elementos: {
+        containerTeclado: null,
+        containerTeclas: null,
+        teclas: []
+    },
+    propiedades: {
+        valor: ""   // aqui va la letra
+    },
+
+    // metodos
+
+    init() {
+        // crear contenedores
+        this.elementos.containerTeclado = document.createElement("div");
+        this.elementos.containerTeclado.setAttribute("id", "teclado");
+        this.elementos.containerTeclas = document.createElement("div");
+        this.elementos.containerTeclas.classList.add("container-teclas");
+
+        //crear la estructura
+        document.body.appendChild(this.elementos.containerTeclado);
+        this.elementos.containerTeclado.appendChild(this.elementos.containerTeclas);
+        this.elementos.containerTeclas.appendChild(this.crearTeclas());
+
+        this.elementos.teclas = this.elementos.containerTeclas.querySelectorAll(".tecla");
+
+    },
+/*
+    mostrarValor(){
+        return this.propiedades.valor;
+    },
+*/
+
+    crearTeclas() {
+
+        //fragment es un doc pequeño donde se guarda mucha info, para cuando
+        //ya este todo guardado en ella, empujarlo de una al html. asi se evita el reflow
+        //Reflow sucede cuando un nav debe procesar y pintar parte de una web nuevamente (f5)
+        const fragment = document.createDocumentFragment();
+        const letras = [
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "a", "s", "d", "f", "g", "h", "j", "k", "l", "ñ",
+            "z", "x", "c", "v", "b", "n", "m"
+        ];
+
+        letras.forEach(letra => {
+            btn = document.createElement("button");
+
+            btn.setAttribute("type", "button");
+            btn.classList.add("tecla");
+
+            btn.textContent = letra;
+            btn.addEventListener("click", (btn) => {
+                this.propiedades.valor = letra;
+
+                // metodos del archivo "metodos.js"
+
+                if (errores < 6) {  // errores es de metodos.js
+
+                    if (!this.haGanado()) {
+                        posicionLetra(letra);   // comprobar que existe la letra en la palabra y guardar las pos donde se encuentra
+                        if (posLetra.length == 0) { // si está vacio, significa q la letra era erronea
+                            errores += 1;
+                            this.pintarLetrasUsadas(letra);
+                        } else{
+                            pintarLetraString(letra);   // modifico string de palabraMostrada
+                            this.pintarLetrasUsadas(letra);
+                            this.pintar();
+                        }
+                    }
+                    
+                    if(this.haGanado()){      // si se ha ganado la partida
+                        // ir a otra pag q sea has ganado
+                        window.location.href = "./hasGanado.html";
+                    }
+
+
+                } else {
+                    // ir a otra pagina q sea "has perdido"
+                    window.location.href = "./hasPerdido.html";
+                }
+
+            });
+
+            fragment.appendChild(btn);  // guardamos en fragment cada btn de letra
+
+            if (letra == "p" || letra == "ñ") {   // mostrar teclado en filas
+                fragment.appendChild(document.createElement("br"));
+            }
+
+        });
+
+        return fragment;
+    },
+
+    pintar() {
+
+        //para que solo aparezca una vez en pantalla
+        document.getElementById("guiones").remove();
+
+        //crear elemento
+        let guiones = document.createElement("p");
+        guiones.setAttribute("id", "guiones");
+        guiones.textContent = palabraMostrada;
+
+        //crear estructura
+        document.getElementById("container-palabraMostrada").appendChild(guiones);
+    },
+
+    pintarLetrasUsadas(letra) {
+        document.getElementById("letras-usadas").textContent += (letra + "  ");
+    },
+
+    //comprobar si la palabra ha sido adivinada (q no quede ningun guion)
+    haGanado() {
+        let result = true;
+        for (let i = 0; i < palabraMostrada.length; i++) {
+            if (palabraMostrada.charAt(i) == '-') {
+                result = false;
+                i = palabraMostrada.length; // para salir del bucle
+            }
+        }
+        return result;
+    }
+};
+
+window.addEventListener("load", teclado.init());
---
+++ proyecto/Ahorcado_trabajando_limpio/metodos.js
@@ -0,0 +1,258 @@
+
+//let rondas; // numero de rondas
+let palabraJugada = ""; // palabra con la que se juega
+let palabraMostrada = ""; // string que ve el jugador en pantalla
+let listaPalabras = new Array(); // array donde se guardan las palabras existentes como Productos
+let jugadores = new Array(); // array donde se guardan los jugadores
+
+let posLetra = new Array(); // array para ir guardando las posiciones de la letra elegida
+//let errores = 0;
+
+class Jugador {
+    constructor(nombre, puntos) {
+        this.nombre = nombre;
+        this.puntos = puntos;
+    }
+
+    getNombre() {
+        return this.nombre;
+    }
+
+    getPuntos() {
+        return this.puntos;
+    }
+
+    setNombre(nombre) {
+        this.nombre = nombre;
+    }
+
+    sumarPunto() {
+        this.puntos += 1;
+    }
+
+}
+
+class Productos {
+    constructor(titulo, anioLanzamiento, director, actores, poster) {
+        this.titulo = titulo;
+        this.anioLanzamiento = anioLanzamiento;
+        this.director = director;
+        this.actores = actores;
+        this.poster = poster;
+    }
+
+    getTitulo() {
+        return this.titulo;
+    }
+
+    getAnioLanzamiento() {
+        return this.anioLanzamiento;
+    }
+
+    getDirector() {
+        return this.director;
+    }
+
+    getActores() {
+        return this.actores;
+    }
+
+    getPoster() {
+        return this.poster;
+    }
+
+    setTitulo(titulo) {
+        this.titulo = titulo;
+    }
+
+    setAnioLanzamiento(anioLanzamiento) {
+        this.anioLanzamiento = anioLanzamiento;
+    }
+
+    setDirector(director) {
+        this.director = director;
+    }
+
+    setActores(actores) {
+        this.actores = actores;
+    }
+
+    setPoster(poster) {
+        this.poster = poster;
+    }
+
+
+
+}
+
+class Partida {
+    constructor(numRondas, jugador1, jugador2){
+        this.numRondas = numRondas;
+        this.rondaActual = 1;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        this.errores = 0;
+    }
+
+    sumarError(){
+        this.errores++;
+    }
+
+    resetearErrores(){
+        this.errores = 0;
+    }
+
+    siguienteRonda(){
+            this.rondaActual++;
+    }
+
+}
+
+
+// crear jugador
+function crearJugador(nombre) {
+    let jugador = new Jugador(nombre, 0);
+    jugadores.push(jugador);
+    return jugador;
+}
+
+// guardar una palabra en el array
+function guardarPalabra(p) {
+    try {
+        palabra = new Productos(p);
+        listaPalabras.push(palabra);
+        palabraJugada = p;
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+// escoger una palabra del array
+function palabraRandom() {
+    let palabra;
+    try {
+        palabra = listaPalabras[Math.random() * (listaPalabras.length - 0) + 0];
+        palabraJugada = palabra.getTitulo();
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+// comprobar que existe la letra en la palabra y guardar las pos donde se encuentra
+function posicionLetra(letra) {
+
+    posLetra = [];  // vaciar el array para que no haya problemas con otras pos de otras letras
+
+    for (let i = 0; i < palabraJugada.length; i++) {
+        if (palabraJugada.charAt(i) == letra) {
+            posLetra.push(i);
+        }
+    }
+}
+
+// pintar los guiones en palabraMostrada
+function pintarGuiones() {
+    palabraMostrada = "";
+    for (let i = 0; i < palabraJugada.length; i++) {
+        if (palabraJugada.charAt(i) != ' ') {
+            palabraMostrada += "-";
+        } else {
+            palabraMostrada += " ";
+        }
+    }
+}
+
+// cambiar un char específico de un string
+function setCharAt(texto, pos, letra) { // texto a cambiar, pos donde queremos cambiar, letra nueva a poner
+    if (pos > texto.length - 1) return texto;
+    return texto.substring(0, pos) + letra + texto.substring(pos + 1);
+}
+
+// pintar letra en pantalla
+function pintarLetraString(letra) {
+
+    for (let i = 0; i < posLetra.length; i++) {
+        palabraMostrada = setCharAt(palabraMostrada, posLetra[i], letra);
+    }
+}
+
+// PARTIDA DOS JUGADORES:
+
+/*
+Implementar la dinámica de juego completa con dos jugadores (de forma local) y el número de partidas a jugar.
+
+Guardar tanto en cookies como en webstorage: el usuario, el número de victorias y el número de partidas realizadas. 
+Se tiene que poder visualizar esta información en algún momento, así como eliminarla.
+
+Implementar las clases necesarias para el desarrollo completo del juego, mínimo se deben crear dos, con sus propiedades y métodos. 
+
+Añadir información extra a producto: año de lanzamiento, director, actores, poster e imágenes. 
+
+*/
+
+
+/*
+
+se guarda en cookies y en webstorage simplemente para poder ir tabajandolas, pero lo logico seria solo usar una, en este caso
+webtorage.
+
+IDEA:
+
+se guardan los dos jugadores  (cookies y webstorage). 
+se solicita el num de rondas.
+jugador1 introduce la primera palabra
+jugador2 juega.
+se dice si jugador 2 gano o perdio
+jugador2 introduce otra palbra
+juega jugador1
+.
+.
+.
+.
+al terminar todas las rondas, se dice quien ha ganado
+se pregunta si se quiere volver a jugar.
+si se juega de nuevo, se borran los datos y se vuelve al principio
+si se quiere dejar de jugar, se borran los datos y se muestra mensaje
+
+
+*/
+
+
+//por mi crearia una clase jugador, pero el profe quiere q se guarde en cookies y en webstorage... asi q no se si crearla o no
+
+function guardarJugador(jugador) {   // jugador es el nombre del jugador
+
+    document.cookie = "jugador-" + jugador + " = " + jugador; // jugador-alanis = alanis
+    localStorage.setItem("jugador-" + jugador, jugador);
+    /*
+    if(document.cookie == null){
+        document.cookie = "jugador1 = " + jugador;
+        localStorage.setItem("jugador1", jugador);        
+    }else{
+        document.cookie = "jugador2 = " + jugador;
+        localStorage.setItem("jugador2", jugador);
+    }
+*/
+}
+
+function guardarVictoriaJugador(jugador, numVictorias) { // jugador es el nombre del jugador
+
+    document.cookie = "jugador-" + jugador + " = " + jugador + "; victorias = " + numVictorias;
+    localStorage.setItem("victoriasJugador-" + jugador, numVictorias);
+
+    /*
+    document,cookie = "jugador1 = " + jugador + "; victorias = " + numVictorias;
+    localStorage.setItem("victoriasJugador1", numVictorias);
+*/
+}
+
+/*
+function guardarVictoriaJug2(jugador, numVictorias){
+    document.cookie = "jugador2 = " + jugador + "; victorias = " + numVictorias;
+    localStorage.setItem("victoriasJugador2", numVictorias);
+}
+
+*/
+
